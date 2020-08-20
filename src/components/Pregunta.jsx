9cad320da65722571ba7e3e455ca487d2c311bcf
dfldiego <dfl.diego92@gmail.com>
2020-08-20 12:57:57 -0300

    agregando state de presupuesto. leyendo el presupuesto del usuario.

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,15 +1,37 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Pregunta = () => {
+
+    // State de Presupuesto
+    const [presupuesto, setPresupuesto] = useState(0);
+
+    // Funcion que lee el presupuesto. 10 -> en base 10
+    const handleChange = e => {
+        setPresupuesto(parseInt(e.target.value, 10));
+    }
+
+    // Funcion submit para definir el presupuesto
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //Validar
+
+        // Si se pasa la validacion
+
+    }
+
     return (
         <Fragment>
 
             <h2>Coloca tu Presupuesto</h2>
-            <form>
+            <form
+                onSubmit={handleSubmit}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={handleChange}
                 />
                 <input
                     type="submit"
