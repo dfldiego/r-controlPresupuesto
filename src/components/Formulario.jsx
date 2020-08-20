c2d382813bc68743fe022a19043d4210da17902e
dfldiego <dfl.diego92@gmail.com>
2020-08-20 15:10:13 -0300

    state para los gastos del usuario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,38 @@
 import React, { Fragment, useState } from 'react'
 
 const Formulario = () => {
+
+    //Creo los states
+    const [nombre, setNombre] = useState('');
+    const [cantidad, setCantidad] = useState(0);
+
+    //Cuando el usuario agrega un gasto
+    const agregarGastos = e => {
+        e.preventDefault();
+
+        // Validar
+
+        //construir el gasto
+
+        //Pasar el gasto al componente principal
+
+        //resetear el form
+
+    }
+
     return (
         <Fragment>
             <h2>Agrega tus gastos aquí</h2>
-            <form>
+            <form
+                onSubmit={agregarGastos}
+            >
                 <label>Nombre Gasto:</label>
                 <input
                     type="text"
                     name="nombreGasto"
                     placeholder="Ej. Transporte"
                     className="u-full-width"
+                    onChange={e => setNombre(e.target.value)}
                 />
                 <label>Cantidad Gasto:</label>
                 <input
@@ -18,6 +40,7 @@ const Formulario = () => {
                     name="cantidadGasto"
                     placeholder="Ej. 300"
                     className="u-full-width"
+                    onChange={e => setCantidad(parseInt(e.target.value, 10))}
                 />
                 <input
                     type="submit"
