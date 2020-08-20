09ec0b1e97e6da0aa2b54746ad25bcec8eb2deaa
dfldiego <dfl.diego92@gmail.com>
2020-08-20 13:19:30 -0300

    validando formulario. validando el presupuesto a ingresar por el usuario.

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -5,6 +5,9 @@ const Pregunta = () => {
     // State de Presupuesto
     const [presupuesto, setPresupuesto] = useState(0);
 
+    // State de error
+    const [error, setError] = useState(false);
+
     // Funcion que lee el presupuesto. 10 -> en base 10
     const handleChange = e => {
         setPresupuesto(parseInt(e.target.value, 10));
@@ -15,8 +18,14 @@ const Pregunta = () => {
         e.preventDefault();
 
         //Validar
+        console.log(presupuesto);
+        if (presupuesto < 1 || isNaN(presupuesto)) {
+            setError(true);
+            return;     //cortar la ejecucion.
+        }
 
         // Si se pasa la validacion
+        setError(false);
 
     }
 
